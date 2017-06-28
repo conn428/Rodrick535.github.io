6c0713bbc5d148a7907efe90a6c19c6b672d90e8
Rodrick535 <rodrick6322@gmail.com>
2017-06-28 09:49:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 if(isset($_POST['submit'])){
     $to = "rodrick6322@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
@@ -15,4 +16,4 @@ if(isset($_POST['submit'])){
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-
+?>
